48f9c932a7b9440218125a5315ff1bb63534e794
Ayano Matsubara <cainaisongyuan7@gmail.com>
2024-02-07 18:06:35 +0900

    add hi page

--- routes/notes.js
+++ routes/notes.js
@@ -4,11 +4,15 @@ var router = express.Router();
 
 const responseObjectDataAll = { textObject1 : {
 id: 1,
-title: 'ノート1のタイトルです', subTitle: 'ノート1のサブタイトルです', bodyText: 'ノート1の本文です'
+title: 'ノート1のタイトルです',
+subTitle: 'ノート1のサブタイトルです',
+bodyText: 'ノート1の本文です'
 },
 textObject2 : {
 id: 2,
-title: 'ノート2のタイトルです', subTitle: 'ノート2のサブタイトルです', bodyText: 'ノート2の本文です'
+title: 'ノート2のタイトルです',
+subTitle: 'ノート2のサブタイトルです',
+bodyText: 'ノート2の本文です'
 },
 };
 /**
